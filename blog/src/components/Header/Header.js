c70baeac573412abf8cabaf7bf73f641bf224326
DJLARRYLOVE <browl191@gmatc.matc.edu>
2025-05-21 15:37:46 -0500

    Setup Project 2

---
+++ blog/src/components/Header/Header.js
@@ -0,0 +1,82 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import styled, { ThemeConsumer } from "styled-components"
+import { Flex } from 'rebass'
+import { H1 } from "../Heading"
+import { SearchButton } from '../Button'
+import { Section } from '../Section'
+
+const StyledHeader = styled.header`
+  margin: 0 auto;
+  padding: var(--space-4) var(--size-gutter);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: ${({ theme }) => theme.variants.header.primary.backgroundColor};
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  margin: 0 10px;
+  color: ${({ theme }) => theme.variants.header.primary.color};
+`
+
+const Image = styled.img`
+  margin: 0;
+`
+
+const Nav = styled(Flex)`
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const Title = styled(H1)`
+  flex: 4;
+`
+
+const MediaQuery = styled.div`
+  @media (max-width: 600px) {
+    display: none;
+  }
+`
+
+const Header = ({ siteTitle }) => (
+  <StyledHeader>
+    <Section flex>
+      <Section width={1/12}
+        flex flexDirection="column" justifyContent="center">
+      <ThemeConsumer>
+        {theme => <Image src={theme.images.mainHeaderImage} />}
+      </ThemeConsumer>
+      </Section>
+      <Section width={11/12}
+        flex flexDirection="column" justifyContent="center">
+        <Nav>
+          <Title>
+            <StyledLink to="/">
+              {siteTitle}       
+            </StyledLink>
+          </Title>
+          <MediaQuery>
+            <StyledLink to="/">Home</StyledLink>
+            <StyledLink to="/about">About</StyledLink>
+            <StyledLink to="/contact">Contact</StyledLink>
+          </MediaQuery>
+          <SearchButton variant='contrast' />
+        </Nav>
+      </Section>    
+    </Section>
+  </StyledHeader>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string
+}
+
+Header.defaultProps = {
+  siteTitle: ""
+}
+
+export { Header }
---
+++ blog/src/pages/index.js
@@ -0,0 +1,80 @@
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import styled from 'styled-components'
+import { Box, Card, Heading } from 'rebass'
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Grid = styled(Box)`
+  display: grid;
+  margin: 0;
+  --w: 280px;
+  --n: 2;
+  gap: var(--size-gap);
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(max(var(--w), 100%/ (var(--n) + 1) + 0.1%), 1fr)
+  );
+  margin-bottom: var(--size-gap);
+  margin-top: var(--size-gap);
+`
+
+const IndexPage = ( {data} ) => (
+  <Layout>
+    <Grid>
+    {
+      data.allContentfulBlogPost.edges.map(edge => (
+        <Card key={edge.node.id}>
+          <Link to={edge.node.slug}>
+            <GatsbyImage
+              image={edge.node.heroImage.gatsbyImageData}
+            />
+          </Link>
+          <Heading>
+            {edge.node.title}
+          </Heading>
+          <div>
+            {edge.node.body.childMarkdownRemark.excerpt}
+          </div>
+        </Card>
+      ))
+    }
+    </Grid>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
+
+export const query = graphql`
+{
+  allContentfulBlogPost {
+    edges {
+      node {
+        id
+        title
+        slug
+        body {
+          childMarkdownRemark {
+            excerpt
+          }
+        }
+        heroImage {
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            width: 600
+          )
+        }
+      }
+    }
+  }
+}
+`
